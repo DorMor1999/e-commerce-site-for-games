f31f7b56cdd1d582f2af63c71357244094ded6d0
Dor Mor <89539078+DorMor1999@users.noreply.github.com>
2022-11-20 14:57:02 +0200

    Add files via upload

---
+++ Game Store/Model/platform.py
@@ -0,0 +1,6 @@
+from Model.product import Product
+
+
+class Platform(Product):
+    def __init__(self, name, price, link_photo_profile, description, publisher, platform, carousel_photos_links):
+        super().__init__(name, price, link_photo_profile, description, publisher, platform, carousel_photos_links)
